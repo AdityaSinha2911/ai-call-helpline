a72acb3e3d56c2387e7bc9d0f5f001f4f93b2bf5
AdityaSinha2911 <adityahjp11111@gmail.com>
2026-03-07 21:27:27 +0530

    Comments are updataed

--- data/prepare.py
+++ data/prepare.py
@@ -12,6 +12,8 @@ Rules:
 
 training_data = []
 
+"""Opening training_data.csv for reading, and storing it in proper format"""
+
 with open('training_data.csv', 'r', encoding='utf-8') as f:
     reader = csv.DictReader(f)
     for row in reader:
@@ -20,6 +22,8 @@ with open('training_data.csv', 'r', encoding='utf-8') as f:
         f'<|system|>\n{SYSTEM_PROMPT}\n<|user|>\n{row["question"]}\n<|assistant|>\n{row["answer"]}'
         }
         training_data.append(example)
+
+"""Opening college_data.json for writing csv file to it"""
 
 with open('college_data.json', 'w', encoding='utf-8') as f:
     json.dump(training_data, f, indent=2, ensure_ascii=False)
